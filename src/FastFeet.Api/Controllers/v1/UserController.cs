c1d8d3a588765a37b3fe87d37bd2b5582c39c353
Uriel Mori <moriuriel@gmail.com>
2024-03-20 20:04:08 -0300

    feat: create endpoint

--- src/FastFeet.Api/Controllers/v1/UserController.cs
+++ src/FastFeet.Api/Controllers/v1/UserController.cs
@@ -3,6 +3,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using FastFeet.Application.Commons.Response;
 using FastFeet.Application.Users.CreateUserCommand;
+using FastFeet.Application.Users.GetUserById;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,6 +13,7 @@ namespace FastFeet.Api.Controllers.v1;
 [ApiController]
 [ApiVersion("1")]
 [Route("api/v{version:apiVersion}/users")]
+[ProducesResponseType(type: typeof(ErrorResponse), statusCode: (int)HttpStatusCode.UnprocessableEntity)]
 public class UserController : ControllerBase
 {
     private readonly IMediator _mediator;
@@ -27,6 +29,17 @@ public class UserController : ControllerBase
     {
         var result = await _mediator.Send(
           request: new CreateUserCommand(idempotencyKey, user),
+          cancellationToken);
+
+        return StatusCode((int)result.HttpStatusCode, result);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(type: typeof(GetUserByIdResponse), statusCode: (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(
+          request: new GetUserByIdQuery(id),
           cancellationToken);
 
         return StatusCode((int)result.HttpStatusCode, result);
